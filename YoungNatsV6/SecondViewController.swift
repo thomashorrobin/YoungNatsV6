60b884a975a5969b715b980f865b9dae544e6a89
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2016-03-22 07:36:41 +1300

    Initial Commit

---
+++ YoungNatsV6/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  YoungNatsV6
+//
+//  Created by Thomas Horrobin on 22/03/16.
+//  Copyright © 2016 Young Nationals NZ. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
