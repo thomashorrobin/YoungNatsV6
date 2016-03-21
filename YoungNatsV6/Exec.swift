457cd3cb34fc49de971f62807d92d7f51a53a7b9
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2016-03-22 07:53:20 +1300

    added exec file

---
+++ YoungNatsV6/Exec.swift
@@ -0,0 +1,26 @@
+//
+//  Exec.swift
+//  YoungNatsV5
+//
+//  Created by Thomas Horrobin on 22/02/16.
+//  Copyright © 2016 Young Nationals NZ. All rights reserved.
+//
+
+import Foundation
+
+class Exec {
+    
+    var uuid: NSUUID
+    var name: String
+    
+    init(name: String){
+        self.uuid = NSUUID()
+        self.name = name
+    }
+    
+    init(city: NSDictionary){
+        self.uuid = NSUUID(UUIDString: (city["Uid"] as! String))!
+        self.name = city["Name"] as! String!
+    }
+    
+}
