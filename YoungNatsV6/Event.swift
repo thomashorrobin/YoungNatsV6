cb2bb1d70d5d655b91d0efa345829b9dba10fbc5
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2016-04-01 14:15:11 -0700

    added event class

---
+++ YoungNatsV6/Event.swift
@@ -0,0 +1,23 @@
+//
+//  Event.swift
+//  YoungNatsV6
+//
+//  Created by Thomas Horrobin on 1/04/16.
+//  Copyright © 2016 Young Nationals NZ. All rights reserved.
+//
+
+import Foundation
+
+class Event {
+    
+    var title: String
+    
+    init(title: String){
+        self.title = title
+    }
+    
+    init(event: NSDictionary){
+        self.title = event["title"] as! String!
+    }
+    
+}
